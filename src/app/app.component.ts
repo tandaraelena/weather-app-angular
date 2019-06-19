81f2c1249ab09066fc865a67af0b8db47361e386
tandaraelena <tandaraelena@yahoo.com>
2019-06-19 19:01:33 +0100

    change the style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, style } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {}
